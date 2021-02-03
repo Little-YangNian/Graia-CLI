b402d3dccc648189694eee4535d1166e681863dc
root <root@localhost.localdomain>
2021-02-03 20:54:36 +0800

    ...

--- build/lib/Graia_CLI/scaffold.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def init_tool(path):
-    os.chdir(path)
-    botpy = requests.get("https://pan-1302360504.cos.ap-chengdu.myqcloud.com/share/HelloGraia.py")
-    with open(f"bot.py",mode="w",encoding="utf-8") as f:
-        f.write(str(botpy.content.decode("utf-8")))
-
